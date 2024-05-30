ab6421e70c40bf5ef0cf37bda6db0bcc785fc003
felipejimeenezz <jimenezluquefelipe@gmail.com>
2024-05-30 19:26:54 +0200

    Interfaz

---
+++ MetodosInterfaz.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+public interface MetodosInterfaz {
+    public void crearTabla();
+    public void rellenarTabla(String empleado, String departamento, double cantidadEuros, String concepto);
+    public void consulta();
+    public void incremento();
+}
